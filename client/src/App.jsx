955eb6a0699131b97e960fe29dabb4a82d19d9da
Ashwin Theagarajan <z5482269@ad.unsw.edu.au>
2023-11-26 22:08:13 +1100

    basic layout for generator page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import GeneratorPage from './Pages/GeneratorPage/GeneratorPage';
 
 function App() {
   return (
     <div className="App">
-      <Sidebar />
+      <GeneratorPage />
     </div>
   );
 }
--- client/src/Pages/GeneratorPage/GeneratorPage.jsx
+++ client/src/Pages/GeneratorPage/GeneratorPage.jsx
@@ -1,9 +1,43 @@
 import React from 'react'
+import './GeneratorPage.css'
 
 function GeneratorPage() {
-  return (
-    <div>GeneratorPage</div>
-  )
-}
+    return (
+        <div className="app-container">
+          <header className="header">
+            <h1>Playlistable.io</h1>
+            <nav className="nav">
+              <a href="/">Home</a>
+              <a href="/about">About</a>
+              {/* Add more navigation links */}
+            </nav>
+          </header>
+          <main className="main-content">
+            <h2>Mood Generator</h2>
+            <p>Describe what mood you're in. Try to be as specific as possible.</p>
+            <input
+              type="text"
+              placeholder="E.g., happy, relaxed, energetic..."
+              className="mood-input"
+            />
+            <button className="generate-button">Generate Playlist</button>
+            <div className="example-moods">
+              <p>Example Moods:</p>
+              <ul>
+                <li>Happy</li>
+                <li>Chill</li>
+                <li>Energetic</li>
+                {/* Add more example moods */}
+              </ul>
+            </div>
+          </main>
+          <footer className="footer">
+            <a href="/privacy">Privacy Policy</a>
+            <a href="/contact">Contact Us</a>
+            {/* Add more footer links */}
+          </footer>
+        </div>
+      );
+    };
 
 export default GeneratorPage
