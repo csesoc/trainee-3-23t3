8f7ef3ae954d462d33655c099f565e9063360a84
Vincent <z5482021@ad.unsw.edu.au>
2023-12-06 14:41:33 +1100

    Started a basic setup for playlist page

--- client/src/Pages/PlaylistPage/PlaylistPage.jsx
+++ client/src/Pages/PlaylistPage/PlaylistPage.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import './PlaylistPage.css'
 
 function PlaylistPage({ code }) {
 
   return (
     <div className="app-container">
       <header className="header">
-        <a href='/' className='header-title'>Playlistable.io</a>
+        <a href='/' className='header-title'>Moodify: Your Playlists</a>
         <nav className="nav">
           <a href="/">Home</a>
           <a href="/about">About</a>
@@ -13,23 +14,7 @@ function PlaylistPage({ code }) {
         </nav>
       </header>
       <main className="main-content">
-        <h2>Mood Generator</h2>
-        <p>Describe what mood you're in. Try to be as specific as possible.</p>
-        <input
-          type="text"
-          placeholder="E.g., generate a playlist for sad boi hours..."
-          className="mood-input"
-        />
-        <button className="generate-button">Generate Playlist</button>
-        <div className="example-moods">
-          <p>Example Moods:</p>
-          <ul>
-            <li>Happy</li>
-            <li>Chill</li>
-            <li>Energetic</li>
-            {/* Add more example moods */}
-          </ul>
-        </div>
+        <h1> <p> You Have No Playlist. Click Playlist Generator to Get Started! </p></h1>
       </main>
       <footer className="footer">
         <a href="/privacy">Privacy Policy</a>
