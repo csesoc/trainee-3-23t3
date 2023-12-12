6101ca940c1bb00ff2d9e5446c5459f701c67591
Shaam <shaamjevan.sj@gmail.com>
2023-12-12 17:53:02 +1100

    implement recommender

--- client/src/Pages/GeneratorPage/GeneratorPage.jsx
+++ client/src/Pages/GeneratorPage/GeneratorPage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import './GeneratorPage.css';
 import axios from 'axios';
+import Fuse from 'fuse.js';
 
 function GeneratorPage() {
 
@@ -20,26 +21,117 @@ function GeneratorPage() {
     }
     setToken(token);
 
-  }, [])
+  }, []);
 
   const searchTracks = async (e) => {
     e.preventDefault();
 
-    // example api call - rn seed artists are travis + drake with genre suited towards gym music
-    const { data } = await axios.get("https://api.spotify.com/v1/recommendations", {
+    // first we get the users top artists - from this we can extract top genres and top artists they listen to
+    const res1 = await axios.get("https://api.spotify.com/v1/me/top/artists", {
       headers: {
         Authorization: `Bearer ${token}`
       },
       params: {
-        seed_artists: '0Y5tJX1MQlPlqiwlOH1tJY,3TVXtAsR1Inumwj472S9r4',
-        seed_genres: 'rap,hip-hop,workout',
-        min_energy: '0.9',
-        min_popularity: '60'
+        limit: '50',
+        time_range: 'medium_term'
       },
     })
-    console.log(data);
 
-    setTracks(data.tracks)
+    // here getting the users top songs and extracting the artists from these songs
+    const res5 = await axios.get("https://api.spotify.com/v1/me/top/tracks", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        limit: '50',
+        time_range: 'medium_term'
+      },
+    })
+    let trackArtists = []
+    res5.data.items.map(item => {
+      for (let artist of item.artists) {
+        trackArtists.push(artist.id);
+      }
+    })
+
+    let topGenres = {};
+    let topArtists = [];
+
+    let minPopularity = 100;
+
+    for (const item of res1.data.items) {
+      if (searchKey.length <= 0) {
+        item.genres.forEach(genre => {
+          genre = genre.replace(/\s+/g, "-");
+          if (topGenres[genre]) {
+            topGenres[genre]++;
+          } else {
+            topGenres[genre] = 1;
+          }
+        });
+      }
+
+      topArtists.push(item.id);
+      if (item.popularity < minPopularity) {
+        minPopularity = item.popularity;
+      }
+    }
+
+    topArtists.concat(topArtists);
+    topArtists = Array.from(new Set(topArtists));
+
+    let sortedGenres = [];
+
+    for (let genre in topGenres) {
+      sortedGenres.push([genre, topGenres[genre]]);
+    }
+    sortedGenres.sort((a, b) => b[1] - a[1]);
+    topGenres = sortedGenres.map(genre => genre[0]);
+
+    // combing user input into our topGenres so we can fuzzy search against those as well
+    if (searchKey.length > 0) {
+      for (let word in searchKey.split(' ')) {
+        topGenres.push(word);
+      }
+    }
+
+    // getting the genre seeds that the recommender API accepts so that we can filter our users top genres into these
+    const res2 = await axios.get("https://api.spotify.com/v1/recommendations/available-genre-seeds", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+    })
+
+    let availableGenres = res2.data.genres;
+
+    // using Fuse fuzzy search to find approximate matches between the users genres and the available genre seeds above
+    const fuse = new Fuse(availableGenres, { keys: ['label'], threshold: 0.2 });
+
+    let filteringGenres = [];
+
+    topGenres.forEach(genre1 => {
+      const result = fuse.search(genre1);
+      if (result.length > 0) {
+        filteringGenres.push(result[0].item);
+      }
+    });
+
+    // using the recommender API with our users top genres and artists => only allows us to choose 5 seeds
+    // in total from seed_artists and seed_genres combined
+    const res3 = await axios.get("https://api.spotify.com/v1/recommendations", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        seed_artists: ''.concat(topArtists.sort(() => Math.random() - 0.5).slice(0, 3)),
+        seed_genres: ''.concat(filteringGenres.sort(() => Math.random() - 0.5).slice(0, 2)),
+        min_popularity: `${minPopularity}`,
+        limit: '20', // TODO: replace this with input from slider
+      },
+    })
+
+    setSearchKey('');
+    setTracks(res3.data.tracks);
   }
 
 
@@ -90,7 +182,7 @@ function GeneratorPage() {
       </footer>
       {token && token.length > 0 &&
         <form onSubmit={searchTracks}>
-          <input type="text" onChange={e => setSearchKey(e.target.value)} />
+          <input type="text" onChange={e => setSearchKey(e.target.value)} value={searchKey} />
           <button type="submit">Search</button>
         </form>}
 
